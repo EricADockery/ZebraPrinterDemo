50b9ccb28eec7896285f5041a42fa46b1d9d13b9
Will Mays <47162632+willm132@users.noreply.github.com>
2021-05-19 09:15:40 -0400

    Update BluetoothTest-Bridging-Header.h
    Added other objc headers needed

--- BluetoothTest/BluetoothTest-Bridging-Header.h
+++ BluetoothTest/BluetoothTest-Bridging-Header.h
@@ -2,4 +2,9 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "MfiBtPrinterConnection.h"
+#import "MFiBtPrinterConnection.h"
+#import "ZebraPrinterConnection.h"
+#import "ZebraPrinter.h"
+#import "ZebraPrinterFactory.h"
+#import "TcpPrinterConnection.h"
+#import "SGD.h"
